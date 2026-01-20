bf9b345dafb8a9afd51ac0203acb31aa05946648
tranxuanba <106661430+tranxuanba@users.noreply.github.com>
2026-01-20 15:10:51 +0700

    Create FoodSkeleton.vue

---
+++ app/components/FoodSkeleton.vue
@@ -0,0 +1,18 @@
+<!-- FoodSkeleton.vue -->
+<template>
+  <v-card class="product-card" elevation="0" variant="outlined" rounded="lg">
+    <!-- Ảnh -->
+    <v-skeleton-loader type="image" aspect-ratio="1" />
+
+    <v-card-text class="text-center pt-3">
+      <v-skeleton-loader type="text" width="80%" class="mx-auto" />
+    </v-card-text>
+
+    <v-card-actions class="px-3 pb-3">
+      <v-skeleton-loader type="text" width="40%" />
+      <v-spacer />
+      <v-skeleton-loader type="avatar" size="32" />
+      <v-skeleton-loader type="avatar" size="32" />
+    </v-card-actions>
+  </v-card>
+</template>
