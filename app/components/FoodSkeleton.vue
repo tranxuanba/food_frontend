532455de13087cf7d73edfb657c8c100672ec04b
tranxuanba <106661430+tranxuanba@users.noreply.github.com>
2026-02-10 13:39:09 +0700

    Update FoodSkeleton.vue

--- app/components/FoodSkeleton.vue
+++ app/components/FoodSkeleton.vue
@@ -8,11 +8,10 @@
       <v-skeleton-loader type="text" width="80%" class="mx-auto" />
     </v-card-text>
 
-    <v-card-actions class="px-3 pb-3">
+    <v-card-actions class="px-3 pb-3 d-flex" style="justify-content: space-between !important;">
       <v-skeleton-loader type="text" width="40%" />
-      <v-spacer />
-      <v-skeleton-loader type="avatar" size="32" />
-      <v-skeleton-loader type="avatar" size="32" />
+      <v-skeleton-loader type="avatar" size="30" />
+      <v-skeleton-loader type="avatar" size="30" />
     </v-card-actions>
   </v-card>
 </template>
